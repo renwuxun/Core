f3c296bdaa9f5e7e97caa15322386dfb7d0a31e2
renwuxun <229602756@qq.com>
2016-06-29 16:38:41 +0800

    有些时候需要对赋值/取值进行拦截

--- Core/Lib/Model.php
+++ Core/Lib/Model.php
@@ -10,4 +10,21 @@
 
 abstract class Core_Lib_Model extends Core_Lib_DataObject {
 
+    public function __set($name, $value) {
+        $m = 'set'.ucfirst($name);
+        if (method_exists($this, $m)) {
+            $this->$m($value);
+        } else {
+            throw new Exception('property '.get_class().'::'.$name.' access deny');
+        }
+    }
+
+    public function __get($name) {
+        $m = 'get'.ucfirst($name);
+        if (method_exists($this, $m)) {
+            $this->$m();
+        } else {
+            throw new Exception('property '.get_class().'::'.$name.' access deny');
+        }
+    }
 }
